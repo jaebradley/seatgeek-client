cdf46968acd606fcfb7bbe5b96ce39d34f174eb3
jaebradley <jae.b.bradley@gmail.com>
2016-09-25 15:39:50 -0400

    added taxonomies

--- src/data/Taxonomy.js
+++ src/data/Taxonomy.js
@@ -105,4 +105,154 @@ Taxonomy.initEnum({
     id: 1060000,
     parent_id: 1000000,
   },
+  NASCAR: {
+    slug: 'nascar',
+    id: 1060100,
+    parent_id: 1060000,
+  },
+  NASCAR_SPRINT_CUP_SERIES: {
+    slug: 'nascar_sprintcup',
+    id: 1060101,
+    parent_id: 1060100,
+  },
+  NASCAR_NATIONWIDE_SERIES: {
+    slug: 'nascar_nationwide',
+    id: 1060102,
+    parent_id: 1060100,
+  },
+  F1_RACING: {
+    slug: 'f1',
+    id: 1060300,
+    parent_id: 1060000,
+  },
+  MONSTER_TRUCK: {
+    slug: 'monster_truck',
+    id: 1060400,
+    parent_id: 1060000,
+  },
+  MOTOCROSS: {
+    slug: 'motocross',
+    id: 1060500,
+    parent_id: 1060000,
+  },
+  GOLF: {
+    slug: 'golf',
+    id: 1070000,
+    parent_id: 1000000,
+  },
+  PGA_GOLF: {
+    slug: 'pga',
+    id: 1070100,
+    parent_id: 1070000,
+  },
+  LPGA_GOLF: {
+    slug: 'lpga',
+    id: 1070200,
+    parent_id: 1070000,
+  },
+  FIGHTING: {
+    slug: 'fighting',
+    id: 1080000,
+    parent_id: 1000000,
+  },
+  BOXING: {
+    slug: 'boxing',
+    id: 1080100,
+    parent_id: 1080000,
+  },
+  MMA: {
+    slug: 'mma',
+    id: 1080200,
+    parent_id: 1080000,
+  },
+  WRESTLING: {
+    slug: 'wrestling',
+    id: 1080300,
+    parent_id: 1080000,
+  },
+  WWE: {
+    slug: 'wwe',
+    id: 1080301,
+    parent_id: 1080300,
+  },
+  ANIMAL_SPORTS: {
+    slug: 'animal_sports',
+    id: 1100000,
+    parent_id: 1000000,
+  },
+  HORSE_RACING: {
+    slug: 'horse_racing',
+    id: 1100100,
+    parent_id: 1100000,
+  },
+  RODEO: {
+    slug: 'rodeo',
+    id: 1100200,
+    parent_id: 1100000,
+  },
+  OLYMPIC_SPORTS: {
+    slug: 'olympic_sports',
+    id: 1120000,
+    parent_id: 1000000,
+  },
+  CONCERT: {
+    slug: '',
+    id: 2000000,
+    parent_id: null,
+  },
+  MUSIC_FESTIVAL: {
+    slug: 'music_festival',
+    id: 2010000,
+    parent_id: 2000000,
+  },
+  THEATER: {
+    slug: 'theater',
+    id: 3000000,
+    parent_id: null,
+  },
+  CLASSICAL: {
+    slug: 'classical',
+    id: 3010000,
+    parent_id: 3000000,
+  },
+  CLASSICAL_OPERA: {
+    slug: 'classical_opera',
+    id: 3010100,
+    parent_id: 3010000,
+  },
+  CLASSICAL_ORCHESTRAL: {
+    slug: 'classical_orchestral_instrumental',
+    id: 3010300,
+    parent_id: 3010000,
+  },
+  CIRQUE_DE_SOLEIL: {
+    slug: 'cirque_du_soleil',
+    id: 3020000,
+    parent_id: 3000000,
+  },
+  BROADWAY_SHOW: {
+    slug: 'broadway_tickets_national',
+    id: 3030000,
+    parent_id: 3000000,
+  },
+  FAMILY_ENTERTAINMENT: {
+    slug: 'family',
+    id: 3050000,
+    parent_id: 3000000,
+  },
+  DANCE_SHOW: {
+    slug: 'dance_performance_tour',
+    id: 3060000,
+    parent_id: 3000000,
+  },
+  FILM: {
+    slug: 'film',
+    id: 3070000,
+    parent_id: 3000000,
+  },
+  LITERARY: {
+    slug: 'literary',
+    id: 3080000,
+    parent_id: 3000000,
+  },
 });
