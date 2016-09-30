34f66384e7b373abb633b22c8e50afed8201a53b
jaebradley <jae.b.bradley@gmail.com>
2016-09-29 23:47:09 -0400

    type checking

--- src/data/request/query/QueryParameterBuilder.js
+++ src/data/request/query/QueryParameterBuilder.js
@@ -1,6 +1,8 @@
 'use es6';
 
 import Unit from '../../Unit';
+import Genre from '../../Genre';
+import Taxonomy from '../../Taxonomy';
 
 export default class QueryParameterBuilder{
 
@@ -42,25 +44,45 @@ export default class QueryParameterBuilder{
     if (!(taxonomies instanceof Array)) {
       throw new Error('taxonomies must be an Array');
     }
-
-    let primaryGenreSlugs = [];
-    for (i = 0; i < primaryGenres.length; i++) {
-
-    }
-    primaryGenres.forEach(genre => primaryGenreSlugs.push(genre.value));
-
-    let otherGenreSlugs = [];
-    otherGenres.forEach(genre => otherGenreSlugs.push(genre.value));
-
-    let taxonomyIds = [];
-    taxonomies.forEach(taxonomy => taxonomyIds.push(taxonomy.id));
-
+    
     return {
       'id': ids,
-      'taxonomies.id': taxonomyIds,
-      'genres[primary].slug': primaryGenreSlugs,
-      'genres.slug': otherGenreSlugs,
+      'taxonomies.id': QueryParameterBuilder.buildTaxonomyIds(taxonomies),
+      'genres[primary].slug': QueryParameterBuilder.buildGenreSlugs(primaryGenres),
+      'genres.slug': QueryParameterBuilder.buildGenreSlugs(otherGenres),
     }
+  }
+
+  static buildGenreSlugs(genres) {
+    if (!(genres instanceof Array)) {
+      throw new Error('genres must be an Array');
+    }
+
+    let genreSlugs = [];
+    for (i = 0; i < genres.length; i++) {
+      let genre = genres[i];
+      if (!(genre instanceof Genre)) {
+        throw new Error('all elements must be a Genre');
+      }
+      genreSlugs.push(genre.value);
+    }
+    return genreSlugs;
+  }
+
+  static buildTaxonomyIds(taxonomies) {
+    if (!(taxonomies instanceof Array)) {
+      throw new Error('taxnomies must be an Array');
+    }
+
+    let taxonomyIds = [];
+    for (i = 0; i < taxonomies.length; i++) {
+      let taxonomy = taxonomies[i];
+      if (!(taxonomy instanceof Taxonomy)) {
+        throw new Error('all elements must be a Taxonomy');
+      }
+      taxonomyIds.push(taxonomy.id);
+    }
+    return taxonomyIds;
   }
 
   static buildPageParameters(perPage, page) {
@@ -113,40 +135,40 @@ export default class QueryParameterBuilder{
   }
 
   static buildPlaceParameters(cityName, stateCode, countryCode, postalCode, queryString) {
-      if ((typeof cityName !== 'undefined') && (typeof cityName !== 'string')) {
-        throw new Error('cityName must be a string value');
-      }
-
-      if ((typeof stateCode !== 'undefined') && (typeof stateCode !== 'string')) {
-        throw new Error('stateCode must be a string of length 2');
-      }
-
-      if ((typeof stateCode === 'string') && (stateCode.length != 2)) {
-        throw new Error('stateCode must be a string of length 2');
-      }
-
-      if ((typeof countryCode !== 'undefined') && (typeof countryCode !== 'string')) {
-        throw new Error('countryCode must be a string of length 2');
-      }
-
-      if ((typeof countryCode === 'string') && (countryCode.length != 2)) {
-        throw new Error('countryCode must be a string of length 2');
-      }
-
-      if ((typeof postalCode !== 'undefined') && (typeof postalCode !== 'string')) {
-        throw new Error('postalCode must be a string value');
-      }
-
-      if ((typeof queryString !== 'undefined') && (typeof queryString !== 'string')) {
-        throw new Error('queryString must be a string value');
-      }
-
-      return {
-        city: cityName,
-        state: stateCode,
-        country: countryCode,
-        postal_code: postalCode,
-        q: queryString,
-      };
+    if ((typeof cityName !== 'undefined') && (typeof cityName !== 'string')) {
+      throw new Error('cityName must be a string value');
     }
+
+    if ((typeof stateCode !== 'undefined') && (typeof stateCode !== 'string')) {
+      throw new Error('stateCode must be a string of length 2');
+    }
+
+    if ((typeof stateCode === 'string') && (stateCode.length != 2)) {
+      throw new Error('stateCode must be a string of length 2');
+    }
+
+    if ((typeof countryCode !== 'undefined') && (typeof countryCode !== 'string')) {
+      throw new Error('countryCode must be a string of length 2');
+    }
+
+    if ((typeof countryCode === 'string') && (countryCode.length != 2)) {
+      throw new Error('countryCode must be a string of length 2');
+    }
+
+    if ((typeof postalCode !== 'undefined') && (typeof postalCode !== 'string')) {
+      throw new Error('postalCode must be a string value');
+    }
+
+    if ((typeof queryString !== 'undefined') && (typeof queryString !== 'string')) {
+      throw new Error('queryString must be a string value');
+    }
+
+    return {
+      city: cityName,
+      state: stateCode,
+      country: countryCode,
+      postal_code: postalCode,
+      q: queryString
+    };
+  }
 };
