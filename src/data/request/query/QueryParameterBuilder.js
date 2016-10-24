cd6a8143f0b21018d12b363787db37adb7e7f9f5
jbradley <jbradley@hubspot.com>
2016-10-24 17:32:31 -0400

    deleting unused methods

--- src/data/request/query/QueryParameterBuilder.js
+++ src/data/request/query/QueryParameterBuilder.js
@@ -30,50 +30,6 @@ export default class QueryParameterBuilder{
     return queryParameters;
   }
 
-  static buildPerformerQueryParameters(ids, slug, primaryGenres, otherGenres, taxonomies,
-                                       parentTaxonomies, queryString, perPage, page) {
-    let queryParameters = {q: queryString};
-    let performerAttributeQuery = new PerformerAttributeQuery(ids, slug, primaryGenres, otherGenres, taxonomies, parentTaxonomies);
-    let paginationQuery = new PaginationQuery(perPage, page);
-    Object.assign(queryParameters,
-                  performerAttributeQuery.buildQueryParameters(),
-                  paginationQuery.buildQueryParameters());
-
-    return queryParameters;
-  }
-
-  static buildGenreSlugs(genres) {
-    if (!(genres instanceof Array)) {
-      throw new Error('genres must be an Array');
-    }
-
-    let genreSlugs = [];
-    for (var i = 0; i < genres.length; i++) {
-      let genre = genres[i];
-      if (!(genre instanceof Genre)) {
-        throw new Error('all elements must be a Genre');
-      }
-      genreSlugs.push(genre.slug);
-    }
-    return genreSlugs;
-  }
-
-  static buildTaxonomyIds(taxonomies) {
-    if (!(taxonomies instanceof Array)) {
-      throw new Error('taxonomies must be an Array');
-    }
-
-    let taxonomyIds = [];
-    for (var i = 0; i < taxonomies.length; i++) {
-      let taxonomy = taxonomies[i];
-      if (!(taxonomy instanceof Taxonomy)) {
-        throw new Error('all elements must be a Taxonomy');
-      }
-      taxonomyIds.push(taxonomy.id);
-    }
-    return taxonomyIds;
-  }
-
   static buildEventsQueryParameters(performerQueryParameters, taxonomyQueryParameters, venueIds, cityName,
                           stateCode, countryCode, postalCode, useIpAddress,
                           latitude, longitude, range, unit, sortOption,
