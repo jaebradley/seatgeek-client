ab4c9de8d13e01afe24c0d39fe22aa51bcfcd9fc
jaebradley <jae.b.bradley@gmail.com>
2016-11-21 16:25:03 -0500

    remove unnecessary values from query parameter builder

--- src/data/request/query/QueryParameterBuilder.js
+++ src/data/request/query/QueryParameterBuilder.js
@@ -9,7 +9,6 @@ import EventVenueLocationQuery from './EventVenueLocationQuery';
 import VenueLocationQuery from './VenueLocationQuery';
 import GeolocationQuery from './GeolocationQuery';
 import PaginationQuery from './PaginationQuery';
-import PerformerAttributeQuery from './PerformerAttributeQuery';
 import PerformerQueryParameter from './PerformerQueryParameter';
 import TaxonomyQueryParameter from './TaxonomyQueryParameter';
 
@@ -51,91 +50,5 @@ export default class QueryParameterBuilder{
                   paginationQuery.buildQueryParameters());
 
     return queryParameters;
-  }
-
-  static buildPerformerQueryParameters(performers) {
-    if (!(performers instanceof Array)) {
-      throw new Error('performers must be an array');
-    }
-
-    let performerQueryParameters = {};
-    for (var i = 0; i < performers.length; i++) {
-      let performer = performers[i];
-
-      if (!(performer instanceof PerformerQueryParameter)) {
-        throw new Error('all elements must be a PerformerQueryParameter');
-      }
-
-      let queryParameterName = performer.buildParameterName();
-      let queryParameterValues = [];
-
-      if (queryParameterName in performerQueryParameters) {
-        queryParameterValues = performerQueryParameters[queryParameterName];
-      }
-
-      queryParameterValues.push(performer.value);
-      performerQueryParameters[queryParameterName] = queryParameterValues;
-    }
-
-    return performerQueryParameters;
-  }
-
-  static buildTaxonomyQueryParameters(taxonomies) {
-    if (!(taxonomies instanceof Array)) {
-      throw new Error('taxonomies must be an array');
-    }
-
-    let taxonomyQueryParameters = {};
-    for (var i = 0; i < taxonomies.length; i++) {
-      let taxonomy = taxonomies[i];
-
-      if (!(taxonomy instanceof TaxonomyQueryParameter)) {
-        throw new Error('all elements must be a TaxonomyQueryParameter');
-      }
-
-      let queryParameterName = taxonomy.buildParameterName();
-      let queryParameterValues = [];
-
-      if (queryParameterName in taxonomyQueryParameters) {
-        queryParameterValues = taxonomyQueryParameters[queryParameterName];
-      }
-
-      queryParameterValues.push(taxonomy.getValue());
-      taxonomyQueryParameters[queryParameterName] = queryParameterValues;
-    }
-
-    return taxonomyQueryParameters;
-  }
-
-  static buildGenreSlugs(genres) {
-    if (!(genres instanceof Array)) {
-      throw new Error('genres must be an Array');
-    }
-
-    let genreSlugs = [];
-    for (var i = 0; i < genres.length; i++) {
-      let genre = genres[i];
-      if (!(genre instanceof Genre)) {
-        throw new Error('all elements must be a Genre');
-      }
-      genreSlugs.push(genre.slug);
-    }
-    return genreSlugs;
-  }
-
-  static buildTaxonomyIds(taxonomies) {
-    if (!(taxonomies instanceof Array)) {
-      throw new Error('taxonomies must be an Array');
-    }
-
-    let taxonomyIds = [];
-    for (var i = 0; i < taxonomies.length; i++) {
-      let taxonomy = taxonomies[i];
-      if (!(taxonomy instanceof Taxonomy)) {
-        throw new Error('all elements must be a Taxonomy');
-      }
-      taxonomyIds.push(taxonomy.id);
-    }
-    return taxonomyIds;
   }
 };
