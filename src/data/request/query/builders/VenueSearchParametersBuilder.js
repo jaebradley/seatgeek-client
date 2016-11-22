f9014568bf5878fde4a0e4e661a23852c4aadb38
jaebradley <jae.b.bradley@gmail.com>
2016-11-22 18:05:12 -0500

    update venue search parameter builder

--- src/data/request/query/builders/VenueSearchParametersBuilder.js
+++ src/data/request/query/builders/VenueSearchParametersBuilder.js
@@ -1,43 +1,64 @@
 'use es6';
 
+import {Map} from 'immutable';
+
 import VenuePropertiesParametersBuilder from './VenuePropertiesParametersBuilder';
 import GeolocationParametersBuilder from './GeolocationParametersBuilder';
 import PaginationParametersBuilder from './PaginationParametersBuilder';
 
+import VenueSearch from '../VenueSearch';
 import VenueProperties from '../VenueProperties';
 import Geolocation from '../Geolocation';
 import Pagination from '../Pagination';
 
 export default class VenueSearchParametersBuilder {
-  static build(query) {
-    let parameters = {
-      ids: query.ids,
-      q: query.queryString,
-    };
-    Object.assign(parameters,
-                  VenuePropertiesParametersBuilder.build(
-                    new VenueProperties({
-                      cityName: query.cityName,
-                      stateCode: query.stateCode,
-                      countryCode: query.countryCode,
-                      postalCode: query.postalCode,
-                    })
-                  ),
-                  GeolocationParametersBuilder.build(
-                    new Geolocation({
-                      useIpAddress: query.useIpAddress,
-                      latitude: query.latitude,
-                      longitude: query.longitude,
-                      range: query.range,
-                      unit: query.unit,
-                    })
-                  ),
-                  PaginationParametersBuilder.build(
-                    new Pagination({
-                      perPage: query.perPage,
-                      page: query.page,
-                    })
-                  ));
+  static build(search) {
+    if (!(search instanceof VenueSearch)) {
+      throw new TypeError('must be a VenueSearch instance');
+    }
+
+    let parameters = Map();
+    if (typeof search.ids !== 'undefined') {
+      parameters = parameters.set(VenueSearchParametersBuilder.getIdsParameterName(),
+                     search.ids);
+    }
+
+    if (typeof search.queryString !== 'undefined') {
+      parameters = parameters.set(VenueSearchParametersBuilder.getQueryStringParameterName(),
+                     search.queryString);
+    }
+
+    parameters = parameters.merge(VenuePropertiesParametersBuilder.build(
+                                    new VenueProperties({
+                                      cityName: search.cityName,
+                                      stateCode: search.stateCode,
+                                      countryCode: search.countryCode,
+                                      postalCode: search.postalCode,
+                                    })
+                                  ),
+                                  GeolocationParametersBuilder.build(
+                                    new Geolocation({
+                                      useIpAddress: search.useIpAddress,
+                                      latitude: search.latitude,
+                                      longitude: search.longitude,
+                                      range: search.range,
+                                      unit: search.unit,
+                                    })
+                                  ),
+                                  PaginationParametersBuilder.build(
+                                    new Pagination({
+                                      perPage: search.perPage,
+                                      page: search.page,
+                                    })
+                                  ));
     return parameters;
+  }
+
+  static getIdsParameterName() {
+    return 'id';
+  }
+
+  static getQueryStringParameterName() {
+    return 'q';
   }
 }
