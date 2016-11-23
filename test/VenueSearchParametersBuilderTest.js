82ab6f82a96762c7516034a0939ca5db988aa4da
jaebradley <jae.b.bradley@gmail.com>
2016-11-23 18:32:34 -0500

    add venue search parameters builder test

---
+++ test/VenueSearchParametersBuilderTest.js
@@ -0,0 +1,16 @@
+'use es6';
+
+import {expect} from 'chai';
+
+import {List} from 'immutable';
+
+import Unit from '../src/data/Unit';
+import Pagination from '../src/data/request/query/Pagination';
+import VenueSearch from '../src/data/request/query/VenueSearch';
+import VenueSearchParametersBuilder from '../src/data/request/query/builders/VenueSearchParametersBuilder';
+
+describe('Tests Venue Search Parameters Builder', function() {
+  it('tests expected behavior for parameter building', function() {
+
+  });
+});
