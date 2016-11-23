40741c09a14725fa7a33a119e3733f3bfbc4bf8d
jaebradley <jae.b.bradley@gmail.com>
2016-11-23 12:02:57 -0500

    updated pagination parameters test

--- test/PaginationParametersBuilderTest.js
+++ test/PaginationParametersBuilderTest.js
@@ -20,4 +20,34 @@ describe('Test pagination parameters builder', function() {
     expect(PaginationParametersBuilder.build(pagination).toJS())
           .to.eql(expectedParameters);
   });
+
+  it('tests non-Pagination input throws Error', function() {
+    expect(() => PaginationParametersBuilder.build(1)).to.throw(TypeError);
+  });
+
+  it('tests Pagination with undefined inputs', function() {
+    let undefinedPerPage = new Pagination({
+      perPage: undefined,
+      page: page,
+    });
+    let undefinedPage = new Pagination({
+      perPage: perPage,
+      page: undefined,
+    });
+    let expectedUndefinedPerPageResult = {
+      page: page
+    };
+    let expectedUndefinedPageResult = {
+      perPage: perPage,
+    };
+    expect(PaginationParametersBuilder.build(undefinedPerPage).toJS())
+          .to.eql(expectedUndefinedPerPageResult);
+    expect(PaginationParametersBuilder.build(undefinedPage).toJS())
+          .to.eql(expectedUndefinedPageResult);
+  });
+
+  it('tests parameter names', function() {
+    expect(PaginationParametersBuilder.getPerPageParameterName()).to.equal('per_page');
+    expect(PaginationParametersBuilder.getPageParameterName()).to.equal('page');
+  });
 });
