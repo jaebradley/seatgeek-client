43f4fff063e8a4d79f0e857a090efbd95b1d6ae2
jaebradley <jae.b.bradley@gmail.com>
2016-11-22 22:54:28 -0500

    added done

--- test/ClientTest.js
+++ test/ClientTest.js
@@ -62,21 +62,25 @@ describe('Test Client', function() {
 
   it('tests genres fetch', function() {
     return SeatGeekClient.getGenres({perPage: 150})
-                 .then(response => expect(response.genres.length).to.equal(Genre.enumValues.length));
+                 .then(response => expect(response.genres.length).to.equal(Genre.enumValues.length))
+                 .done();
   });
 
   it('tests taxonomies fetch', function() {
     return SeatGeekClient.getTaxonomies({perPage: 100})
-                 .then(response => expect(response.taxonomies.length).to.equal(Taxonomy.enumValues.length));
+                 .then(response => expect(response.taxonomies.length).to.equal(Taxonomy.enumValues.length))
+                 .done();
   });
 
   it('tests venues fetch', function() {
     return SeatGeekClient.getVenues({cityName: 'Boston'})
-                 .then(response => expect(response.venues.length).to.equal(100));
+                 .then(response => expect(response.venues.length).to.equal(100))
+                 .done();
   });
 
   it('tests clients fetch', function() {
     return SeatGeekClient.getEvents()
                  .then(response => console.log(response))
+                 .done();
   })
 });
