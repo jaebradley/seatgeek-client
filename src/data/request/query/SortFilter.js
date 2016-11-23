109100e1ad786851cd0f266bcdb6dda143c83b47
jaebradley <jae.b.bradley@gmail.com>
2016-11-22 19:47:05 -0500

    updated sort filter default

--- src/data/request/query/SortFilter.js
+++ src/data/request/query/SortFilter.js
@@ -6,8 +6,8 @@ import SortOption from './SortOption';
 import SortDirection from './SortDirection';
 
 let defaults = {
-  option: SortOption.ID,
-  direction: SortDirection.ASC,
+  option: SortOption.SCORE,
+  direction: SortDirection.DESC,
 };
 
 export default class SortFilter extends Record(defaults) {
