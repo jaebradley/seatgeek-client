9f080c12c6da6824b4e5dd1319ba16aa7054361d
jaebradley <jae.b.bradley@gmail.com>
2016-11-25 12:03:30 -0500

    flattened pagination

--- src/data/request/query/EventsSearch.js
+++ src/data/request/query/EventsSearch.js
@@ -5,17 +5,19 @@ import {List, Record} from 'immutable';
 import Pagination from './Pagination';
 import SortFilter from './SortFilter';
 import Geolocation from './Geolocation';
-import VenuesFilter from './VenuesFilter';
+import VenueProperties from './VenueProperties';
+import Constants from '../../Constants';
 
 let defaults = {
   ids: List(),
   venues: new VenuesFilter(),
   performers: List(),
   taxonomies: List(),
+  filters: List(),
   geolocation: new Geolocation(),
   sort: new SortFilter(),
-  filters: List(),
-  pagination: new Pagination(),
+  perPage: Constants.getDefaultPerPage(),
+  page: Constants.getDefaultPage(),
 };
 
 export default class EventsSearch extends Record(defaults) {
