8b4eb85d8ea781ba8dc6c1d0a8c2da0f03625acb
jbradley <jbradley@hubspot.com>
2016-12-07 11:34:10 -0500

    move parameter logic to venues fitler parameters builder

--- src/data/request/query/builders/VenuesFilterParametersBuilder.js
+++ src/data/request/query/builders/VenuesFilterParametersBuilder.js
@@ -3,18 +3,37 @@
 import {Map} from 'immutable';
 
 import VenuesFilter from '../VenuesFilter';
-import EventsVenuePropertiesParametersBuilder from './EventsVenuePropertiesParametersBuilder';
 
 export default class VenuesFilterParametersBuilder {
   static build(filter) {
+    if (!(filter instanceof VenuesFilter)) {
+      throw new TypeError('filter is not a VenuesFilter');
+    }
+
     let parameters = Map();
     if (typeof filter.ids !== 'undefined') {
       parameters = parameters.set(VenuesFilterParametersBuilder.getIdsParameterName(),
                                   filter.ids);
     }
 
-    if (typeof filter.properties !== 'undefined') {
-      parameters = parameters.merge(EventsVenuePropertiesParametersBuilder.build(filter.properties));
+    if (typeof filter.cityName !== 'undefined') {
+      parameters = parameters.set(VenuesFilterParametersBuilder.getCityNamePropertyName(),
+                                  filter.cityName);
+    }
+
+    if (typeof filter.stateCode !== 'undefined') {
+      parameters = parameters.set(VenuesFilterParametersBuilder.getStateCodePropertyName(),
+                                  filter.stateCode);
+    }
+
+    if (typeof filter.countryCode !== 'undefined') {
+      parameters = parameters.set(VenuesFilterParametersBuilder.getCountryCodePropertyName(),
+                                  filter.countryCode);
+    }
+
+    if (typeof filter.postalCode !== 'undefined') {
+      parameters = parameters.set(VenuesFilterParametersBuilder.getPostalCodePropertyName(),
+                                  filter.postalCode);
     }
 
     return parameters;
@@ -22,5 +41,21 @@ export default class VenuesFilterParametersBuilder {
 
   static getIdsParameterName() {
     return 'venue.id';
+  }
+
+  static getCityNamePropertyName() {
+    return 'venue.city';
+  }
+
+  static getStateCodePropertyName() {
+    return 'venue.state';
+  }
+
+  static getCountryCodePropertyName() {
+    return 'venue.country';
+  }
+
+  static getPostalCodePropertyName() {
+    return 'venue.postal_code';
   }
 };
