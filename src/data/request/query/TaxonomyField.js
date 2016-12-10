7da0101a5613b00a765ad6f727265e89bd7b1037
jaebradley <jae.b.bradley@gmail.com>
2016-12-10 10:36:49 -0500

    rename field name

--- src/data/request/query/TaxonomyField.js
+++ src/data/request/query/TaxonomyField.js
@@ -5,6 +5,6 @@ import {Enum} from 'enumify';
 export default class TaxonomyField extends Enum {}
 TaxonomyField.initEnum({
   NAME: { value: 'name', parameterName: 'name' },
-  ID: { value: 'id', parameterValue: 'slug' },
-  PARENT_ID: { value: 'parent_id', parameterValue: 'parent__id' },
+  ID: { value: 'id', parameterName: 'slug' },
+  PARENT_ID: { value: 'parent_id', parameterName: 'parent__id' },
 });
