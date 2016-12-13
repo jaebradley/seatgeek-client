9cda7e9f9ec04a7a8ef908e135b1a8f98aa0ab5e
jbradley <jbradley@hubspot.com>
2016-12-13 09:29:40 -0500

    update client test

--- test/ClientTest.js
+++ test/ClientTest.js
@@ -5,28 +5,29 @@ import {Genre} from '../src/index';
 import {Taxonomy, TaxonomyField} from '../src/index';
 
 describe('Test Client', function() {
+  let client = new SeatGeekClient();
   it('tests genres fetch', function() {
-    return SeatGeekClient.getGenres({perPage: 150})
+    return client.getGenres({perPage: 150})
                  .then(response => expect(response.genres.length).to.equal(Genre.enumValues.length));
   });
 
   it('tests taxonomies fetch', function() {
-    return SeatGeekClient.getTaxonomies({perPage: 100})
+    return client.getTaxonomies({perPage: 100})
                  .then(response => expect(response.taxonomies.length).to.equal(Taxonomy.enumValues.length));
   });
 
   it('tests venues fetch', function() {
-    return SeatGeekClient.getVenues({cityName: 'Boston'})
+    return client.getVenues({cityName: 'Boston'})
                  .then(response => console.log(response));
   });
 
   it('tests events fetch', function() {
-    return SeatGeekClient.getEvents({venues: {properties: {cityName: 'Boston', stateCode: 'MA'}}})
+    return client.getEvents({venues: {properties: {cityName: 'Boston', stateCode: 'MA'}}})
                  .then(response => console.log(response));
   });
 
   it('tests events fetch', function() {
-    return SeatGeekClient.getEvents({taxonomies: [{
+    return client.getEvents({taxonomies: [{
         taxonomy: Taxonomy.CONCERT,
         field: TaxonomyField.NAME,
       }]})
