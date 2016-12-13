586c4ee1ffd88d4e17c32549baa4b850f673882a
jbradley <jbradley@hubspot.com>
2016-12-13 18:37:42 -0500

    remove imports

--- src/query/events/EventsSearch.js
+++ src/query/events/EventsSearch.js
@@ -2,11 +2,9 @@
 
 import {List, Record} from 'immutable';
 
-import Constants from '../../data/Constants';
 import Geolocation from '../Geolocation';
 import Pagination from '../Pagination';
 import SortFilter from './sort/SortFilter';
-import VenuesFilter from './venue/VenuesFilter';
 import VenuesProperties from '../VenuesProperties';
 
 let defaults = {
