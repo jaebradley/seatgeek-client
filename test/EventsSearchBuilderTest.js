2b57db492e351a4f7d4efbae79edcabd27f6ab60
jaebradley <jae.b.bradley@gmail.com>
2016-12-18 15:40:36 -0500

    adding integration test

--- test/EventsSearchBuilderTest.js
+++ test/EventsSearchBuilderTest.js
@@ -6,17 +6,23 @@ chai.use(chaiImmutable);
 
 import {List} from 'immutable';
 
+import EventsVenuesProperties from '../src/query/events/EventsVenuesProperties';
+import Geolocation from '../src/query/Geolocation';
 import Filter from '../src/query/events/filter/Filter';
 import FilterOption from '../src/query/events/filter/FilterOption';
 import Operator from '../src/query/events/filter/Operator';
 import Unit from '../src/data/Unit';
 import EventsSearch from '../src/query/events/EventsSearch';
+import Pagination from '../src/query/Pagination';
 import PerformerField from '../src/query/events/performer/PerformerField';
 import PerformerFilter from '../src/query/events/performer/PerformerFilter';
 import PerformerSpecificity from '../src/query/events/performer/PerformerSpecificity';
 import SortFilter from '../src/query/events/sort/SortFilter';
 import SortOption from '../src/query/events/sort/SortOption';
 import SortDirection from '../src/query/events/sort/SortDirection';
+import Taxonomy from '../src/data/Taxonomy';
+import TaxonomyField from '../src/query/TaxonomyField';
+import TaxonomyFilter from '../src/query/TaxonomyFilter';
 import VenuesProperties from '../src/query/VenuesProperties';
 
 import EventsSearchBuilder from '../src/query/events/EventsSearchBuilder';
@@ -29,27 +35,52 @@ describe('Test Event Search Builder', function() {
   let value1 = 1;
   let value2 = 2;
 
+  let performerFiltersJson = [
+    {
+      value: value1
+    },
+    {
+      value: value2,
+      field: PerformerField.ID,
+      specificity: PerformerSpecificity.HOME_TEAM
+    }
+  ];
+  let expectedPerformerFilters = List.of(
+    new PerformerFilter({
+      value: value1
+    }),
+    new PerformerFilter({
+      value: value2,
+      field: PerformerField.ID,
+      specificity: PerformerSpecificity.HOME_TEAM
+    }),
+  );
+  let filtersJson = [
+    {
+      value: value1
+    },
+    {
+      value: value2,
+      option: FilterOption.HIGHEST_PRICE,
+      operator: Operator.LESS_THAN,
+    }
+  ];
+  let expectedFilters = List.of(
+    new Filter({
+      value: value1
+    }),
+    new Filter({
+      value: value2,
+      option: FilterOption.HIGHEST_PRICE,
+      operator: Operator.LESS_THAN,
+    })
+  );
+  let sortJson = {
+    'option': SortOption.SCORE,
+    'direction': SortDirection.ASCENDING,
+  };
+  let expectedSort = new SortFilter(sortJson);
   it('tests performers filter building', function() {
-    let performerFiltersJson = [
-      {
-        value: value1
-      },
-      {
-        value: value2,
-        field: PerformerField.ID,
-        specificity: PerformerSpecificity.HOME_TEAM
-      }
-    ];
-    let expectedPerformerFilters = List.of(
-      new PerformerFilter({
-        value: value1
-      }),
-      new PerformerFilter({
-        value: value2,
-        field: PerformerField.ID,
-        specificity: PerformerSpecificity.HOME_TEAM
-      }),
-    );
     let createdPerformerFilters = EventsSearchBuilder.buildPerformerFilters(performerFiltersJson);
     chai.expect(createdPerformerFilters).to.eql(expectedPerformerFilters);
   });
@@ -75,26 +106,6 @@ describe('Test Event Search Builder', function() {
   });
 
   it('tests filters building', function() {
-    let filtersJson = [
-      {
-        value: value1
-      },
-      {
-        value: value2,
-        option: FilterOption.HIGHEST_PRICE,
-        operator: Operator.LESS_THAN,
-      }
-    ];
-    let expectedFilters = List.of(
-      new Filter({
-        value: value1
-      }),
-      new Filter({
-        value: value2,
-        option: FilterOption.HIGHEST_PRICE,
-        operator: Operator.LESS_THAN,
-      })
-    );
     let createdFilters = EventsSearchBuilder.buildFilters(filtersJson);
     chai.expect(createdFilters).to.eql(expectedFilters);
   });
@@ -120,12 +131,7 @@ describe('Test Event Search Builder', function() {
   });
 
   it('tests sort filter builder', function() {
-    let filterJson = {
-      'option': SortOption.SCORE,
-      'direction': SortDirection.ASCENDING,
-    };
-    let expectedFilter = new SortFilter(filterJson);
-    chai.expect(EventsSearchBuilder.buildSortFilter(filterJson)).to.eql(expectedFilter);
+    chai.expect(EventsSearchBuilder.buildSortFilter(sortJson)).to.eql(expectedSort);
   });
 
   it('tests sort filter building error cases', function() {
@@ -139,4 +145,62 @@ describe('Test Event Search Builder', function() {
     };
     chai.expect(() => EventsSearchBuilder.buildSortFilter(wrongDirection)).to.throw(TypeError);
   });
+
+  it('tests complete events search building', function() {
+    let json = {
+      ids: ids,
+      venueIds: [4, 5, 6],
+      performers: performerFiltersJson,
+      taxonomies: [
+        {
+          taxonomy: Taxonomy.CONCERT,
+          field: TaxonomyField.ID,
+        }
+      ],
+      filters: filtersJson,
+      sort: sortJson,
+      cityName: 'Boston',
+      stateCode: 'MA',
+      countryCode: 'US',
+      postalCode: '12345',
+      useIpAddress: false,
+      latitude: 6789,
+      longitude: 1234,
+      range: 15,
+      unit: Unit.MILE,
+      perPage: 11,
+      page: 12
+    };
+    let expectedSearch = new EventsSearch({
+      ids: ids,
+      performers: expectedPerformerFilters,
+      taxonomies: List.of(
+        new TaxonomyFilter({
+          taxonomy: Taxonomy.CONCERT,
+          field: TaxonomyField.ID
+        })
+      ),
+      filters: expectedFilters,
+      sort: expectedSort,
+      venues: new EventsVenuesProperties({
+        ids: [4, 5, 6],
+        cityName: 'Boston',
+        stateCode: 'MA',
+        countryCode: 'US',
+        postalCode: '12345'
+      }),
+      geolocation: new Geolocation({
+        useIpAddress: false,
+        latitude: 6789,
+        longitude: 1234,
+        range: 15,
+        unit: Unit.MILE
+      }),
+      pagination: new Pagination({
+        perPage: 11,
+        page: 12
+      })
+    })
+    chai.expect(EventsSearchBuilder.build(json))
+  })
 });
